9f71c35a80c4c0382108a5561a8fa2c23a4b04bb
Cpt-ZZZ <FearMeHorror@gmail.com>
2025-09-09 03:01:22 +0800

    added to Data cleaning

--- PFDA Project.R
+++ PFDA Project.R
@@ -20,6 +20,31 @@ arData <- read.csv("iata_airport_codes.csv")
 flightData_clean <- distinct(flightData)
 flightData_clean
 
+fix_military_time <- function(time_col) 
+{
+  # Extract hours and minutes
+  hours <- time_col %/% 100
+  minutes <- time_col %% 100
+  
+  # Correct minutes > 59 by subtracting 60 and adding 1 to hours
+  overflow_min <- minutes > 59
+  minutes[overflow_min] <- minutes[overflow_min] - 60
+  hours[overflow_min] <- hours[overflow_min] + 1
+  
+  # Correct hours > 23 by subtracting 24
+  hours <- ifelse(hours > 23, hours - 24, hours)
+  
+  # Recombine hours and minutes
+  time_col <- hours * 100 + minutes
+  
+  # Set negative values to NA
+  time_col <- ifelse(time_col < 0, NA, time_col)
+  
+  return(time_col)
+}
+test_times <- c(1230, 2400, 1679, -50, 2359)
+fix_military_time(test_times)
+
 #	TAXI_OUT: The time duration elapsed between departure from the ORIGIN_AIRPORT and WHEELS_OFF.
 flightData_clean$TAXI_OUT <- ifelse(is.na(flightData_clean$TAXI_OUT), mean(flightData_clean$TAXI_OUT, na.rm =  TRUE),
                                     round(flightData_clean$TAXI_OUT,1))
@@ -38,10 +63,11 @@ flightData_clean$WHEELS_OFF <- ifelse(is.na(flightData_clean$WHEELS_OFF), mean(f
 flightData_clean$DEPARTURE_TIME <- ifelse(is.na(flightData_clean$DEPARTURE_TIME) & !is.na(flightData_clean$WHEELS_OFF) & !is.na(flightData_clean$TAXI_OUT),
                                           (flightData_clean$WHEELS_OFF- flightData_clean$TAXI_OUT),
                                           round(flightData_clean$DEPARTURE_TIME,1))
-flightData_clean
+
 #DEPARTURE_DELAY: Total delay on departure.
-flightData_clean$DEPARTURE_DELAY <- ifelse(is.na(flightData_clean$DEPARTURE_DELAY), mean(flightData_clean$DEPARTURE_DELAY, na.rm =  TRUE),
-                                           round(flightData_clean$DEPARTURE_DELAY,1))
+flightData_clean$DEPARTURE_DELAY <- ifelse(is.na(flightData_clean$DEPARTURE_DELAY) & !is.na(flightData_clean$SCHEDULED_DEPARTURE) & !is.na(flightData_clean$DEPARTURE_TIME),
+                                           (flightData_clean$SCHEDULED_DEPARTURE- flightData_clean$DEPARTURE_TIME),
+                                           round(flightData_clean$DEPARTURE_DELAY))
 
 #AIR_TIME: Time duration between WHEELS_OFF and WHEELS_ON time.
 flightData_clean$AIR_TIME <- ifelse(is.na(flightData_clean$AIR_TIME) & !is.na(flightData_clean$WHEELS_ON) & !is.na(flightData_clean$WHEELS_OFF),
@@ -65,6 +91,21 @@ flightData_clean$ARRIVAL_DELAY <- ifelse(is.na(flightData_clean$ARRIVAL_DELAY) &
                                          (flightData_clean$ARRIVAL_TIME - flightData_clean$SCHEDULED_ARRIVAL),
                                          round(flightData_clean$ARRIVAL_DELAY,1))
 
+
+flightData_clean$SCHEDULED_DEPARTURE <- fix_military_time(flightData_clean$SCHEDULED_DEPARTURE)
+flightData_clean$DEPARTURE_TIME <- fix_military_time(flightData_clean$DEPARTURE_TIME)
+flightData_clean$SCHEDULED_ARRIVAL <- fix_military_time(flightData_clean$SCHEDULED_ARRIVAL)
+flightData_clean$ARRIVAL_TIME <- fix_military_time(flightData_clean$ARRIVAL_TIME)
+flightData_clean$WHEELS_OFF <- fix_military_time(flightData_clean$WHEELS_OFF)
+flightData_clean$WHEELS_ON <- fix_military_time(flightData_clean$WHEELS_ON)
+
+#Ensure that it is four numbers making up the time data
+#flightData_clean$DEPARTURE_TIME <- sprintf("%04d", flightData_clean$DEPARTURE_TIME)
+#flightData_clean$ARRIVAL_TIME <- sprintf("%04d", flightData_clean$ARRIVAL_TIME)
+
 #check missing Data
 is.na(flightData_clean)
 sum(is.na(flightData_clean))
+
+invalid_times <- subset(flightData_clean, DEPARTURE_TIME > 2359 | ARRIVAL_TIME > 2359)
+nrow(invalid_times)  # Should be 0 if everything's clean
